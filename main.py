677a03aa4b830aebfda3f70e03e5755f27f78012
Justas Urbonas <mr.urbonas@gmail.com>
2022-04-05 19:51:39 +0300

    test deploy

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ app = FastAPI(docs_url="/")
 
 app.include_router(products_router)
 
+# test
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(get_env(API_PORT)), reload=True)
