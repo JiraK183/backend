af003818bc2063f8ea8b456f6d795f0e1a961c3e
Justas Urbonas <mr.urbonas@gmail.com>
2022-04-28 21:42:14 +0300

    purchase product, my products

--- features/products/products_router.py
+++ features/products/products_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from features.auth.auth_service import get_current_user
+from features.auth.models import CurrentUser
 from features.products import products_service
 from features.products.dtos import CreateProductRequest
 
@@ -14,16 +15,23 @@ async def get_products():
     return {"products": products_service.get_products()}
 
 
-# @products_router.get("/ids")
-# async def get_products_by_ids(product_ids: list[OID] = Query(None)):
-#     return {"products": products_service.get_products_by_ids(product_ids)}
-
-
 @products_router.post("/")
 async def create_product(create_product_request: CreateProductRequest):
     return products_service.create_product(create_product_request)
 
 
+@products_router.get("/my")
+async def get_my_products(current_user: CurrentUser = Depends(get_current_user)):
+    return {"products": products_service.get_my_products(current_user)}
+
+
 @products_router.delete("/{product_id}/", status_code=204)
 async def delete_product(product_id: str):
     products_service.delete_product(product_id)
+
+
+@products_router.post("/{product_id}/purchase/")
+async def purchase_product(
+    product_id: str, current_user: CurrentUser = Depends(get_current_user)
+):
+    return products_service.purchase_product(product_id, current_user)
--- features/products/products_service.py
+++ features/products/products_service.py
@@ -3,6 +3,7 @@ from typing import Union, Optional
 from bson import ObjectId
 from fastapi import HTTPException
 
+from features.auth.models import CurrentUser
 from features.products.dtos import CreateProductRequest
 from features.products.models import Product
 from models import OID
@@ -11,10 +12,14 @@ from utils import mongo_client
 products_collection = mongo_client.products
 
 
-def get_products() -> list[Product]:
-    product_dicts = products_collection.find()
+def get_products(match: dict = None) -> list[Product]:
+    product_dicts = products_collection.find(match)
 
     return [__parse_product(product) for product in product_dicts]
+
+
+def get_my_products(current_user: CurrentUser) -> list[Product]:
+    return get_products({"ownedBy": current_user.username})
 
 
 def get_products_by_ids(
@@ -50,6 +55,19 @@ def delete_product(product_id: str) -> None:
 
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Product not found.")
+
+
+def purchase_product(product_id: str, current_user: CurrentUser) -> None:
+    product = products_collection.find_one(
+        {"_id": ObjectId(product_id), "ownedBy": current_user.username}
+    )
+
+    if product is not None:
+        raise HTTPException(status_code=400, detail="You may not buy an item twice.")
+
+    products_collection.update_one(
+        {"_id": ObjectId(product_id)}, {"$push": {"ownedBy": current_user.username}}
+    )
 
 
 def __parse_product(product: dict) -> Product:
